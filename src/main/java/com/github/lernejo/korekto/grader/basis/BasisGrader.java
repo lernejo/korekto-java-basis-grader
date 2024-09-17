b111b7d04a4f1de472d4b5736018eb123fdc50bb
ledoyen <ledoyen.loic@gmail.com>
2024-09-17 02:51:11 +0200

    Upgrade to KTK 1.4.4 (handle CI permission error)

--- src/main/java/com/github/lernejo/korekto/grader/basis/BasisGrader.java
+++ src/main/java/com/github/lernejo/korekto/grader/basis/BasisGrader.java
@@ -1,23 +1,17 @@
 package com.github.lernejo.korekto.grader.basis;
 
 import com.github.lernejo.korekto.grader.basis.parts.Part1Grader;
-import com.github.lernejo.korekto.toolkit.GradePart;
 import com.github.lernejo.korekto.toolkit.Grader;
 import com.github.lernejo.korekto.toolkit.GradingConfiguration;
 import com.github.lernejo.korekto.toolkit.PartGrader;
-import com.github.lernejo.korekto.toolkit.misc.HumanReadableDuration;
 import com.github.lernejo.korekto.toolkit.partgrader.GitHubActionsPartGrader;
 import com.github.lernejo.korekto.toolkit.partgrader.JacocoCoveragePartGrader;
 import com.github.lernejo.korekto.toolkit.partgrader.MavenCompileAndTestPartGrader;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.util.Collection;
 import java.util.List;
 
 public class BasisGrader implements Grader<LaunchingContext> {
-
-    private final Logger logger = LoggerFactory.getLogger(BasisGrader.class);
 
     @Override
     public String name() {
@@ -39,22 +33,7 @@ public class BasisGrader implements Grader<LaunchingContext> {
         context.getGradeDetails().getParts().addAll(grade(context));
     }
 
-    private Collection<? extends GradePart> grade(LaunchingContext context) {
-        return graders().stream()
-            .map(g -> applyPartGrader(context, g))
-            .toList();
-    }
-
-    private GradePart applyPartGrader(LaunchingContext context, PartGrader<LaunchingContext> g) {
-        long startTime = System.currentTimeMillis();
-        try {
-            return g.grade(context);
-        } finally {
-            logger.debug("{} in {}", g.name(), HumanReadableDuration.toString(System.currentTimeMillis() - startTime));
-        }
-    }
-
-    private Collection<? extends PartGrader<LaunchingContext>> graders() {
+    public Collection<PartGrader<LaunchingContext>> graders() {
         return List.of(
             new Part1Grader("Part 1 - Git History", 4.0D, 0.5D),
             new MavenCompileAndTestPartGrader<>(
